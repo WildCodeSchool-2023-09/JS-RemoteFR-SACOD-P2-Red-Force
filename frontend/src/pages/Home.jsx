720bfdc01b031e17557d8d103d9e9b4ef4486ff3
batsave <batsave.twitch@gmail.com>
2023-10-25 17:37:43 +0200

    📦 NEW: Add Background Video

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
-
 import hometitles from "../data/homepage-titles.json";
+import bgvideo from "../assets/mp4/home-background.mp4";
+import "../scss/home.scss";
 
 export default function Home() {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -15,13 +16,28 @@ export default function Home() {
 
   return (
     <main>
-      <p>{hometitles[currentIndex].t1}</p>
-      <p>{hometitles[currentIndex].t2}</p>
-      <p>{hometitles[currentIndex].t3}</p>
-      <p>{hometitles[currentIndex].t4}</p>
-      <p>{hometitles[currentIndex].t5}</p>
-      <p>{hometitles[currentIndex].t6}</p>
-      <p>{hometitles[currentIndex].t7}</p>
+      <div className="homemain">
+        <video
+          className="background-video"
+          autoPlay
+          loop
+          controls={false}
+          playsInline
+          muted
+        >
+          <source src={bgvideo} type="video/mp4" />
+        </video>
+        <div className="container">
+          <div className="titleContainer">
+            <p className="title a1">{hometitles[currentIndex].t1}</p>
+            <p className="title a2">{hometitles[currentIndex].t2}</p>
+            <p className="title a3">{hometitles[currentIndex].t3}</p>
+            <p className="title a4">{hometitles[currentIndex].t4}</p>
+            <p className="title a5">{hometitles[currentIndex].t5}</p>
+            <p className="title a6">{hometitles[currentIndex].t6}</p>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
