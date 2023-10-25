174461aa177fb373ef244036dd060aab3067bf71
Océane Lahoual <138216585+Midunighto@users.noreply.github.com>
2023-10-25 17:17:39 +0200

    Navbar responsive start

--- frontend/src/components/Burger.jsx
+++ frontend/src/components/Burger.jsx
@@ -2,10 +2,6 @@ import "../scss/burger.scss";
 import burger from "../assets/icon_burger.svg";
 
 function Burger() {
-  return (
-    <div className="burger">
-      <img src={burger} alt="" />
-    </div>
-  );
+  return <img className="burger" src={burger} alt="" />;
 }
 export default Burger;
