718be91bd1594741cc05955260ddc9d33a6ccaa6
Océane Lahoual <138216585+Midunighto@users.noreply.github.com>
2023-10-26 17:26:23 +0200

    Navbar responsive & burger menu ok

--- frontend/src/components/Burger.jsx
+++ frontend/src/components/Burger.jsx
@@ -1,7 +1,16 @@
 import "../scss/burger.scss";
+import PropTypes from "prop-types";
 import burger from "../assets/icon_burger.svg";
 
-function Burger() {
-  return <img className="burger" src={burger} alt="" />;
+function Burger({ handleOpen }) {
+  return (
+    <button className="burger-button" onClick={handleOpen} type="button">
+      <img className="burger" src={burger} alt="" />
+    </button>
+  );
 }
+Burger.propTypes = {
+  handleOpen: PropTypes.func.isRequired,
+};
+
 export default Burger;
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,25 +1,47 @@
 import { NavLink as Link } from "react-router-dom";
+import { useState } from "react";
 import navdata from "../data/navdata.json";
 import Burger from "./Burger";
 import "../scss/navbar.scss";
 import trivalitylogo from "../assets/Trivality_Logo_Officiel.svg";
 
 export default function Navbar() {
+  /* State to open burger menu */
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => {
+    setOpen(!open);
+  };
+  const [openCat, setOpenCat] = useState(false);
+  const handleOpenCat = () => {
+    setOpenCat(!openCat);
+  };
+
   return (
     <nav>
       <img className="trivality" src={trivalitylogo} alt="" />
       <div className="navbar">
-        <ul className="nav">
+        <ul className={`nav ${open ? "nav-open" : "hide-nav"}`}>
           {navdata.map((index) =>
             index.dropdown !== undefined ? (
               <li className="dropdown-link" key={index.id}>
-                <Link className="nav-link" to={index.linkurl}>
+                <Link
+                  className="nav-link"
+                  onClick={handleOpenCat}
+                  to={index.linkurl}
+                >
                   {index.linkname} <span>▾</span>
                 </Link>
-                <ul className="dropdown-content">
+                <ul
+                  className={`dropdown-content ${
+                    openCat ? "cat-open" : "hide-cat"
+                  }`}
+                >
                   {index.dropdown.map((dropdownindex) => (
-                    <li key={dropdownindex.id}>
-                      <Link className="nav-link" to={dropdownindex.linkurl}>
+                    <li className="nav-ddlink" key={dropdownindex.id}>
+                      <Link
+                        className="nav-link nav-ddlink"
+                        to={dropdownindex.linkurl}
+                      >
                         {dropdownindex.linkname}
                       </Link>
                     </li>
@@ -35,7 +57,7 @@ export default function Navbar() {
             )
           )}
         </ul>
-        <Burger />
+        <Burger handleOpen={handleOpen} />
       </div>
     </nav>
   );
