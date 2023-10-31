d80015178783743c2a1a3323781d5ab65ac4098b
Batsave <111178330+Batsave@users.noreply.github.com>
2023-10-31 13:20:45 +0100

    Fix ESLint

--- frontend/src/pages/Quiz.jsx
+++ frontend/src/pages/Quiz.jsx
@@ -2,7 +2,6 @@ import axios from "axios";
 import { useState } from "react";
 import QuizCard from "../components/QuizCard";
 import "../scss/root.scss";
-
 const initialQuestionData = {
   question: "",
   correct_answer: "",
@@ -10,7 +9,6 @@ const initialQuestionData = {
 };
 export default function Quiz() {
   const [answersValue, setAnswersValue] = useState(initialQuestionData);
-
   const getQuestion = () => {
     axios.get("https://opentdb.com/api.php?amount=1").then((response) => {
       setAnswersValue(response.data.results[0]);
@@ -19,7 +17,6 @@ export default function Quiz() {
   const responses = answersValue.incorrect_answers.concat([
     answersValue.correct_answer,
   ]); 
-  
   return (
     <div>
       <QuizCard
