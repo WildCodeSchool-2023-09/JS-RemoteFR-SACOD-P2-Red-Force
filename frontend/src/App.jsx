63c8b1cd8cac833cdf6a6c380bd114c1244a5cb0
batsave <batsave.twitch@gmail.com>
2023-11-06 15:49:16 +0100

    🐛 FIX: Pages Bugs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,18 @@
 import Navbar from "./components/Navbar";
 import "./scss/root.scss";
 import Home from "./pages/Home";
-import Quiz from "./pages/Quiz";
 import Footer from "./components/Footer";
 import "./scss/Footer.scss";
 
 function App() {
   return (
-    <main className="main-master">
+    <>
       <Navbar />
-      <Home />
+      <main className="main-master">
+        <Home />
+      </main>
       <Footer />
-      <Quiz />
-    </main>
+    </>
   );
 }
 
